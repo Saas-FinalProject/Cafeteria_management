fe445e5726732371f8f334a770e845c7ebdaf0a5
HISSANDANISH <hissandanish@gmail.com>
2020-05-09 10:54:27 +0530

    Commit,by danish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
-class User < ActiveRecord::Base
-  has_many :orders
+class User < ApplicationRecord
   has_secure_password
+  has_many :orders
   validates :name, presence: true
   validates :email, presence: true
   validates :address, presence: true
