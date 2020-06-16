07e3917ccd134534bbf9950dca86f0def76bb0f0
karthik51199 <karthik51199@gmail.com>
2020-06-16 13:49:17 +0530

    Added attribute question_id to answers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_150824) do
+ActiveRecord::Schema.define(version: 2020_06_16_081427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "solution"
+    t.bigint "question_id"
   end
 
   create_table "applicants", force: :cascade do |t|
