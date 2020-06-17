c9b1df7d541d73441b25acc9bddad550852ab3e9
karthik51199 <karthik51199@gmail.com>
2020-06-17 14:03:27 +0530

    Added attribute :name to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_081427) do
+ActiveRecord::Schema.define(version: 2020_06_16_085432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_081427) do
 
   create_table "questions", force: :cascade do |t|
     t.string "query"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
