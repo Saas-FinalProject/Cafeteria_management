0a31a276c7ea69bb684005f9e4f044b2187ca487
karthik51199 <karthik51199@gmail.com>
2020-06-25 20:56:01 +0530

    Fixed bug in Increment, Decrement and Add buttons in both index.html.erb(menus) and cart.html.erb

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,22 +1,4 @@
 class OrderItemsController < ApplicationController
-  # def change
-  #   order_item = Order.find(session[:current_order_id]).order_items.find_by(menu_item_id: params[:id])
-  #   if params[:quantity].to_i > 0
-  #     if order_item
-  #       order_item.quantity = params[:quantity]
-  #       order_item.save
-  #     else
-  #       menu_item = MenuItem.find(params[:id])
-  #       OrderItem.create!(order_id: session[:current_order_id], menu_item_name: menu_item.name, menu_item_id: menu_item.id, menu_item_price: menu_item.price, quantity: params[:quantity])
-  #     end
-  #   else
-  #     if order_item
-  #       order_item.destroy
-  #     end
-  #   end
-  #   redirect_to menus_path
-  # end
-
   def decrement
     order_item = Order.find(session[:current_order_id]).order_items.find_by(menu_item_id: params[:id])
     if order_item.quantity > 1
